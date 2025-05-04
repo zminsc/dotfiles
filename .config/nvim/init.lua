65caaf27797e65c2e12405a952a4c74c30b116a5
Steven Chang <zminsc@seas.upenn.edu>
2025-05-04 14:19:28 -0400

    feat: add keymap for :Neogit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -134,6 +134,15 @@ require("lazy").setup({
 					"nvim-telescope/telescope.nvim", -- optional
 				},
 				opts = {},
+				keys = {
+					{
+						"<leader>g",
+						function()
+							require("neogit").open()
+						end,
+						desc = "Open Neogit",
+					},
+				},
 			},
 		},
 
