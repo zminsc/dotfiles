4e2db8d06325557ffb16219595f2cebf719682a0
Steven Chang <zminsc@seas.upenn.edu>
2026-03-30 17:11:37 -0400

    fix(nvim): make nvim-treesitter 0.12.0-compatible

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,9 @@
 return { -- treesitter
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	lazy = false,
 	opts = {
 		ensure_installed = { "lua", "vim" },
 		auto_install = true,
 	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
 }
