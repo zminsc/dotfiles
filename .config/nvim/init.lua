70d31c5fca78c3514f5a3fe8a612424b2b3a83da
Steven Chang <zminsc@seas.upenn.edu>
2026-03-31 16:03:52 -0400

    feat(nvim): specify formatters for markdown/html

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -56,7 +56,9 @@ require("lazydev").setup({
 require("conform").setup({
 	format_on_save = { timeout_ms = 500, lsp_fallback = true },
 	formatters_by_ft = {
+		html = { "prettierd" },
 		lua = { "stylua" },
+		markdown = { "prettierd" },
 		javascript = { "prettierd" },
 	},
 })
